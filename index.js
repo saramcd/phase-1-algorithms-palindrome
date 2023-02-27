1c827d615687eae3e50673af79e4e2ae0de6c730
Sara McDonnell <saramcdonnell32@gmail.com>
2023-02-27 09:46:40 -0500

    lab

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const wordArray = word.split("");
+  const reverseWordArray = [];
+  for (let i = wordArray.length - 1; i >= 0; i--) {
+    reverseWordArray.push(wordArray[i]);
+  }
+  return wordArray.toString() == reverseWordArray.toString();
 }
 
 /* 
   Add your pseudocode here
+  
+  new array from word
+  iterate through array from the end, pushing to another new array
+  compare arrays are the same
 */
 
 /*
@@ -13,6 +23,12 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hello"));
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
